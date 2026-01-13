5585d58aea8292bba9bdafbabd25a7773e1176ea
wubin28 <wubinben@icloud.com>
2026-01-13 22:12:14 +0800

    feat: replaced 'hello, world' with '你好，世界'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black">
         <h1 className="text-6xl font-bold text-black dark:text-zinc-50">
-          Hello World
+          你好，世界
         </h1>
       </main>
     </div>
